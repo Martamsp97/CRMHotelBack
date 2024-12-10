dba58f2a4989e9727e4da9a77e4b6d658046bbb0
Juan Castaneda <jccastanedacamargo@outlook.com>
2024-12-10 11:47:54 +0100

    Se crean Controllers de habitaciones, con sus respectivas peticiones y rutas funcionales

--- src/routes/api/habitaciones.routes.js
+++ src/routes/api/habitaciones.routes.js
@@ -1,3 +1,18 @@
+const { getAll, create, getById, update, destroy } = require('../../controllers/habitacion.controller');
+
 const router = require('express').Router();
 
-module.exports = {}
+router.get('/', getAll);
+
+router.get('/:roomId', getById );
+
+router.post('/', create);
+
+router.put('/:roomId', update );
+
+router.delete('/:roomId', destroy);
+
+
+
+
+module.exports = router;
