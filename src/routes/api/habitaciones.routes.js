c80c90390dde911f4940d9edad5fecf1abad8014
Marta Sánchez <marta.msp97@gmail.com>
2024-12-11 15:09:47 +0100

    solucioné conflictos que estaba teniendo con la unión del develop de nuria y el mio

--- src/routes/api/habitaciones.routes.js
+++ src/routes/api/habitaciones.routes.js
@@ -2,14 +2,13 @@ const { getAll, create, getById, update, destroy } = require('../../controllers/
 
 const router = require('express').Router();
 
-<<<<<<< HEAD
 router.get('/', getAll);
 
-router.get('/:roomId', getById );
+router.get('/:roomId', getById);
 
 router.post('/', create);
 
-router.put('/:roomId', update );
+router.put('/:roomId', update);
 
 router.delete('/:roomId', destroy);
 
@@ -17,6 +16,4 @@ router.delete('/:roomId', destroy);
 
 
 module.exports = router;
-=======
-module.exports = router
->>>>>>> a1957c907d39840e6c1938c561d7ee26039c3b37
+
