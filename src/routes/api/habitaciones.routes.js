2552849ce65615c83925de81d9ba16f307b6c2a8
Juan Castaneda <jccastanedacamargo@outlook.com>
2024-12-17 09:00:09 +0100

    Modificacion archivos env, routes

--- src/routes/api/habitaciones.routes.js
+++ src/routes/api/habitaciones.routes.js
@@ -1,20 +1,34 @@
-const { getAll, create, getById, update, destroy, getHabByPiso } = require("../../controllers/habitacion.controller");
-
+const {
+  getAll,
+  create,
+  getById,
+  update,
+  destroy,
+} = require("../../controllers/habitacion.controller");
 const { checkHabId } = require("../../middlewares/habitaciones.middleware");
-
-const { checkAdmin, checkToken } = require("../../middlewares/usuarios.middlewares");
+const {
+  checkAdmin,
+  checkToken,
+} = require("../../middlewares/usuarios.middlewares");
 
 const router = require("express").Router();
 
 router.get("/", getAll);
 
-router.get("/:roomId", getById);
-router.get('/piso/:piso', getHabByPiso);
-router.get('/:roomId', checkHabId, getById);
-router.post('/', checkToken, checkAdmin, create);
-router.put('/:roomId', checkToken, checkAdmin, checkHabId, update);
-router.delete('/:roomId', checkToken, checkAdmin, checkHabId, destroy);
+// router.get("/piso/:piso", getHabByPiso);
 
+router.get("/:roomId", checkHabId, getById);
 
+router.post("/", checkToken, checkAdmin, create);
+
+router.put("/:roomId", checkToken, checkAdmin, checkHabId, update);
+
+router.delete("/:roomId", checkToken, checkAdmin, checkHabId, destroy);
+
+router.post("/", create);
+
+router.put("/:roomId", update);
+
+router.delete("/:roomId", destroy);
 
 module.exports = router;
